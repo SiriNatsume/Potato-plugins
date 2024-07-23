157631aee4e6ce78604de7dc8efe7fc249668c4f
hydrogen <hydrogens@petalmail.com>
2024-07-23 15:33:25 +0800

    2024.07.23 update

---
+++ nonebot_plugin_sticker_saver/clock.py
@@ -0,0 +1,35 @@
+import json
+
+
+def save_data_to_json(data):
+    with open("data/sticker_saver/group.json", "w") as json_file:
+        json.dump(data, json_file)
+
+
+def load_data_from_json():
+    try:
+        with open("data/sticker_saver/group.json", "r") as json_file:
+            data = json.load(json_file)
+        return data
+    except FileNotFoundError:
+        return []
+
+
+def group_add(gid):
+    data = load_data_from_json()
+    if gid not in data:
+        data.append(gid)
+        save_data_to_json(data)
+        return 1
+    else:
+        pass
+
+
+def group_del(gid):
+    data = load_data_from_json()
+    if gid not in data:
+        return 0
+    else:
+        data.remove(gid)
+        save_data_to_json(data)
+        return 1
